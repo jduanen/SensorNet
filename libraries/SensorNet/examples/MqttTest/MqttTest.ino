1be595ceae49142a0a37b45427321eeac0f27bc8
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-07-17 17:39:30 -0700

    Changed to reflect the new SensorNet library interface.

--- libraries/SensorNet/examples/MqttTest/MqttTest.ino
+++ libraries/SensorNet/examples/MqttTest/MqttTest.ino
@@ -1,13 +1,16 @@
 #include "SensorNet.h"
 #include "wifi.h"
 
+#define REPORT_INTERVAL 60000  // 60 secs
+
 #define MQTT_SERVER   "192.168.166.113"
 #define MQTT_PORT     1883
 
+unsigned long lastReport = 0;
 String msg;
 int i = 0;
 
-SensorNet sn;
+SensorNet sn("MQTT_TEST");
 
 void callback(char *topic, byte *payload, unsigned int length) {
   Serial.println("Message arrived in topic: " + String(topic));
@@ -21,19 +24,26 @@ void callback(char *topic, byte *payload, unsigned int length) {
   Serial.println("-----------------------");
 }
 
+void callbackX(char *, byte *, unsigned int) {
+  Serial.println("MMMMM");
+}
+
 void setup() {
   sn.serialStart(&Serial, 9600, true);
   sn.wifiStart(WLAN_SSID, WLAN_PASS);
 
-  sn.mqttStart(MQTT_SERVER, MQTT_PORT, "/sensors/test");
+  sn.mqttSetup(MQTT_SERVER, MQTT_PORT, "/sensors/test");
   sn.mqttSub(callback);
 }
 
 void loop() {
-  SensorNet::WIFI_STATE state = sn.wifiState();
-  Serial.println(state.macAddr);
+  sn.mqttRun();
 
-  msg = "testing" + String(i++);
-  sn.mqttPub(msg);
-  delay(3000);
+  unsigned long now = millis();
+  if ((now - lastReport) > REPORT_INTERVAL) {
+    msg = "testing" + String(i++);
+    sn.mqttPub(msg);
+    Serial.println("Pub: " + msg);
+    lastReport = now;
+  }
 }

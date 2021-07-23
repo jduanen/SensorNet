7852d55af7247e7dc7e10a8ac14b5a198227e82a
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-07-22 18:43:58 -0700

    Created.

---
+++ tools/SensorPlot/SensorPlot.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+'''
+SensorPlot: Gather and plot data from SensorNet log files
+'''
+
+import argparse
+import json
+import logging
+import os
+import sys
+
+from plotters import PLOTTERS
+
+
+DEFAULTS = {
+    'logLevel': "INFO",  #"DEBUG"  #"WARNING",
+    'samplesFile': "/home/jdn/Data/SensorNet/sensornet.log"
+}
+
+
+def run(options):
+    with open(options.samplesFile, 'r') as f:
+        samples = f.readlines()
+    timestamps = []
+    values = []
+    for sample in samples:
+        parts = sample.split(" ")
+        if parts[1].startswith(options.plotter.topicPrefix):
+            timestamps.append(parts[0])
+            values.append(parts[2].strip().split(','))
+    options.plotter.plot(timestamps, values)
+
+
+def getOpts():
+    usage = f"Usage: {sys.argv[0]} [-v] [-L <logLevel>] [-l <logFile>] " + \
+      "[-s <samplesFile>] sensor"
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "-L", "--logLevel", action="store", type=str,
+        default=DEFAULTS['logLevel'],
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Logging level")
+    ap.add_argument(
+        "-l", "--logFile", action="store", type=str,
+        help="Path to location of logfile (create it if it doesn't exist)")
+    ap.add_argument(
+        "-s", "--samplesFile", action="store", type=str,
+        default=DEFAULTS['samplesFile'],
+        help="Path to location of file with sensor samples")
+    ap.add_argument(
+        "-v", "--verbose", action="count", default=0,
+        help="Enable printing of debug info")
+    ap.add_argument(
+        "sensor", action="store", type=str, choices=PLOTTERS.keys(),
+        help="Type of Sensor")
+    opts = ap.parse_args()
+
+    if opts.logFile:
+        logging.basicConfig(filename=opts.logFile,
+                            format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=opts.logLevel)
+    else:
+        logging.basicConfig(level=opts.logLevel,
+                            format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+    if not os.path.exists(opts.samplesFile):
+        logging.error(f"Samples file '{opts.samplesFile}' doesn't exist")
+        sys.exit(1)
+
+    opts.plotter = PLOTTERS[opts.sensor]
+
+    if opts.verbose:
+        print(f"    Sensor Type:  {opts.plotter.description}")
+        print(f"    Samples File: {opts.samplesFile}")
+    return opts
+
+
+if __name__ == '__main__':
+    opts = getOpts()
+    r = run(opts)
+    sys.exit(r)
---
+++ tools/SensorPlot/plotters.py
@@ -0,0 +1,130 @@
+'''
+SensorPlot SensorNet Data Plotting Package
+'''
+
+import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
+
+
+NUM_TICKS = 20
+
+
+'''
+Base Class for SensorPlot Plotters
+'''
+class Plotter(ABC):
+    #### TODO think about using Pandas
+    def __init__(self, topicPrefix, description):
+        self.topicPrefix = topicPrefix
+        self.description = description
+
+    @abstractmethod
+    def plot(self, timestamps, values):
+        pass
+
+'''
+SensorPlot Plotter for Radition Monitor
+'''
+class RadPlotter(Plotter):
+    def __init__(self):
+        super().__init__("/sensors/Radiation/", "Radiation sensor (SBT-11A)")
+
+    def plot(self, timestamps, values, window=60):
+        cpm, uSv, volts = zip(*values)
+
+        uSv = list(map(float, uSv))
+        fig, ax = plt.subplots()
+        plt.xlabel("Sample Time")
+        plt.ylabel("uSv per hour")
+        ax.plot(timestamps, uSv, color='b')
+        ax.tick_params(axis='y', labelcolor='black')
+
+        if window:
+            assert window <= len(uSv), "Window too large for data size"
+            avg = [sum(uSv[i:i+window])/window for i in range(len(uSv) - window)]
+            ax.plot(avg, color='r')
+
+        ticks = round(len(values) / (NUM_TICKS * 60)) * 60
+        plt.xticks(timestamps[::ticks], rotation='vertical')
+        plt.gcf().autofmt_xdate()
+        plt.show()
+
+
+'''
+SensorPlot Plotter for Air Quality Monitor with PMS Sensor
+'''
+class PmsPlotter(Plotter):
+    def __init__(self):
+        super().__init__("/sensors/AirQuality/PMS/", "Air quality sensor (PMS7003)")
+
+    def plot(self, timestamps, values):
+        pms1, pms2_5, pms10 = zip(*values)
+
+        pms1 = list(map(int, pms1))
+        pms2_5 = list(map(int, pms2_5))
+        pms10 = list(map(int, pms10))
+
+        plt.xlabel("Sample Time")
+        plt.ylabel("Particle Count")
+        plt.plot(timestamps, pms1, color='b', label="pms1", linewidth=1.0)
+        plt.plot(timestamps, pms2_5, color='g', label="pms2.5", linewidth=1.0)
+        plt.plot(timestamps, pms10, color='r', label="pms10", linewidth=1.0)
+        plt.tick_params(axis='y', labelcolor='black')
+        ticks = round(len(values) / (NUM_TICKS * 60)) * 60
+        plt.xticks(timestamps[::ticks], rotation='vertical')
+        plt.gcf().autofmt_xdate()
+        plt.legend()
+        plt.show()
+
+
+'''
+SensorPlot Plotter for Air Quality Monitor with SPS Sensor
+'''
+class SpsPlotter(Plotter):
+    def __init__(self):
+        super().__init__("/sensors/AirQuality/SPS/", "Air quality sensor (SPS30)")
+
+    def plot(self, timestamps, values, TPS=True):
+        pm1, pm2_5, pm4, pm10, nc0_5, nc1, nc2_5, nc4, nc10, tps = zip(*values)
+
+        pm1 = list(map(float, pm1))
+        pm2_5 = list(map(float, pm2_5))
+        pm4 = list(map(float, pm4))
+        pm10 = list(map(float, pm10))
+        nc0_5 = list(map(float, nc0_5))
+        nc1 = list(map(float, nc1))
+        nc2_5 = list(map(float, nc2_5))
+        nc4 = list(map(float, nc4))
+        nc10 = list(map(float, nc10))
+        tps = list(map(float, tps))
+
+        fig, ax = plt.subplots()
+        ax.plot(pm2_5, color='blue', label="pm2.5", linewidth=1.0)
+        ax.plot(pm10, color='cyan', label="pm10", linewidth=1.0)
+        ax.plot(nc0_5, color='orange', label="nc0.5", linewidth=1.0)
+        ax.plot(nc2_5, color='brown', label="nc2.5", linewidth=1.0)
+        ax.plot(nc10, color='magenta', label="nc10", linewidth=1.0)
+        ax.tick_params(axis='y', labelcolor='black')
+        ax.set_xlabel("Sample Time")
+        ax.set_ylabel("Particulate Concentration")
+        ax.legend(loc="upper left")
+
+        if TPS:
+            ax2 = ax.twinx()
+            ax2.plot(tps, color='r', label="avg particle size", linewidth=1.0)
+            ax2.tick_params(axis='y', labelcolor='r')
+            ax.set_ylabel("Typical Particle Size")
+            ax2.legend(loc="upper right")
+
+        ticks = round(len(values) / (NUM_TICKS * 60)) * 60
+#        plt.xlabel("Sample Time")
+#        plt.xticks(timestamps[::ticks], rotation='vertical')
+#        plt.gcf().autofmt_xdate()
+        plt.show()
+
+
+PLOTTERS = {
+  "rad": RadPlotter(),
+  "pms": PmsPlotter(),
+  "sps": SpsPlotter()
+}

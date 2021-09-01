e925d6b8b8202efabb15b8ef32c3166e251ea167
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-09-01 16:07:33 -0700

    Added callback features. Version 1.0.1.

--- sensors/Radiation/Radiation.ino
+++ sensors/Radiation/Radiation.ino
@@ -6,26 +6,57 @@
 #include "wifi.h"
 
 #define APP_NAME        "Radiation"
-#define APP_VERSION     "1.0.0"
+#define APP_VERSION     "1.0.1"
 #define REPORT_SCHEMA   "CPM:d,uSv/h:.4f,Vcc:.2f"
 
 #define TOPIC_PREFIX    "/sensors/Radiation"
 
-#define REPORT_INTERVAL 60000  // one report every 60 secs
+#define REPORT_INTERVAL 60000  // one report every minute -- defined by sensor
 
 #define MQTT_SERVER     "192.168.166.113"
 #define MQTT_PORT       1883
 
+
+unsigned int reportInterval = DEF_REPORT_INTERVAL;
+
 SensorNet sn(APP_NAME, APP_VERSION, REPORT_SCHEMA);
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  sn.consolePrintln("Message arrived in topic: " + String(topic));
 
-  sn.consolePrint("Message: ");
+void callback(char* topic, byte* payload, unsigned int length) {
+  byte *cmdPtr = payload;
+  byte *valPtr = NULL;
+  String top, cmd, val;
+
+  payload[length] = '\0';
   for (int i = 0; i < length; i++) {
-    sn.consolePrint(String(payload[i]));
+    if (payload[i] == '=') {
+      cmdPtr[i] = '\0';
+      valPtr = &payload[i + 1];
+    }
   }
-  sn.consolePrintln("");
+  top = String(topic);
+  cmd = String((char *)cmdPtr);
+  val = String((char *)valPtr);
+
+  sn.consolePrintln(top + ", " + cmd + ", " + val);
+
+  String msg;
+  if (cmd.equals("RSSI")) {
+    SensorNet::WIFI_STATE wifiState = sn.wifiState();
+    msg = "RSSI=" + String(wifiState.rssi);
+    sn.consolePrintln(msg);
+    sn.mqttPub(msg);
+  } else if (cmd.equals("rate")) {
+    if (val == NULL) {
+      msg = "rate=" + String(DEF_REPORT_INTERVAL);
+      sn.consolePrintln(msg);
+      sn.mqttPub(msg);
+    } else {
+      sn.consolePrintln("ERROR: unable to set rate");
+    }
+  } else {
+    sn.consolePrintln("ERROR: unknown command (" + cmd + ")");
+  }
 }
 
 void setup() {
@@ -36,7 +67,7 @@ void setup() {
   sn.wifiStart(WLAN_SSID, WLAN_PASS);
 
   sn.mqttSetup(MQTT_SERVER, MQTT_PORT, TOPIC_PREFIX);
-  ////sn.mqttSub(callback);
+  sn.mqttSub(callback);
 }
 
 void loop() {
@@ -44,6 +75,7 @@ void loop() {
 
   sn.mqttRun();
 
+  // N.B. the sensor dictates the sample rate
   if (Serial.available() > 0) {
     inMsg = Serial.readStringUntil('\n');
     Serial.println("> " + inMsg);

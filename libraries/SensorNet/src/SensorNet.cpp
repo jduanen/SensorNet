f721231d9dada896af44f8c1e2154b5106111c86
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-08-31 19:19:49 -0700

    Fixed formatting.

--- libraries/SensorNet/src/SensorNet.cpp
+++ libraries/SensorNet/src/SensorNet.cpp
@@ -6,155 +6,156 @@
 #include "SensorNet.h"
 
 SensorNet::SensorNet() {
-	consolePtr = NULL;
+    consolePtr = NULL;
 }
 
 SensorNet::SensorNet(String name) {
-	consolePtr = NULL;
-	appName = name;
+    consolePtr = NULL;
+    appName = name;
 }
 
 SensorNet::SensorNet(String name, String version) {
-	consolePtr = NULL;
-	appName = name;
-	appVersion = version;
+    consolePtr = NULL;
+    appName = name;
+    appVersion = version;
 }
 
 SensorNet::SensorNet(String name, String version, String schema) {
-	consolePtr = NULL;
-	appName = name;
-	appVersion = version;
-	reportSchema = schema;
+    consolePtr = NULL;
+    appName = name;
+    appVersion = version;
+    reportSchema = schema;
 }
 
 // Initialize a serial port
 //// TODO figure out default parameters
 void SensorNet::serialStart(HardwareSerial *portPtr, uint16 baud, bool console) {
-	portPtr->begin(baud);
-	portPtr->println("INIT SERIAL");
-	if (console) {
-		consolePtr = portPtr;
-	}
+    portPtr->begin(baud);
+    portPtr->println("INIT SERIAL");
+    if (console) {
+        consolePtr = portPtr;
+    }
 }
 
 // Print on console (if one is enabled)
 void SensorNet::consolePrint(String str) {
-  if (consolePtr != NULL) {
-  	consolePtr->print(str);
-	}
+    if (consolePtr != NULL) {
+        consolePtr->print(str);
+    }
 }
 
 // Println on console (if one is enabled)
 void SensorNet::consolePrintln(String str) {
-  if (consolePtr != NULL) {
-  	consolePtr->println(str);
-	}
+    if (consolePtr != NULL) {
+        consolePtr->println(str);
+    }
 }
 
 // Wait for input on console (if one is enabled)
 void SensorNet::consoleWaitForInput() {
-  if (consolePtr != NULL) {
-    while (!consolePtr->available());
-    while (consolePtr->available()) Serial.read();
-	}
+    if (consolePtr != NULL) {
+        while (!consolePtr->available());
+        while (consolePtr->available()) Serial.read();
+    }
 }
 
 // Initialize the WiFi connection
 void SensorNet::wifiStart(String ssid, String password) {
-  WiFi.begin(ssid, password);
-	consolePrint("Starting WIFI...");
-  delay(1000);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    consolePrintln("Connecting to WiFi.." + String(WiFi.status()));
-  }
+    WiFi.begin(ssid, password);
+    consolePrint("Starting WIFI...");
+    delay(1000);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        consolePrintln("Connecting to WiFi.." + String(WiFi.status()));
+    }
 
-  _ipAddr = WiFi.localIP();
-  WiFi.macAddress(_mac);
-  _macAddr = String(_mac[0], HEX) + ":" + 
-             String(_mac[1], HEX) + ":" +
-             String(_mac[2], HEX) + ":" +
-             String(_mac[3], HEX) + ":" +
-             String(_mac[4], HEX) + ":" +
-             String(_mac[5], HEX);
-	String(appName + "_" + _macAddr).toCharArray(_clientName, BUF_SIZE);
-
-  consolePrintln("Connected to the WiFi network: " + _macAddr + " @ " + _ipAddr.toString());
+    _ipAddr = WiFi.localIP();
+    WiFi.macAddress(_mac);
+    _macAddr = String(_mac[0], HEX) + ":" +
+               String(_mac[1], HEX) + ":" +
+               String(_mac[2], HEX) + ":" +
+               String(_mac[3], HEX) + ":" +
+               String(_mac[4], HEX) + ":" +
+               String(_mac[5], HEX);
+    String(appName + "_" + _macAddr).toCharArray(_clientName, BUF_SIZE);
+    consolePrintln("Connected to the WiFi network: " + _macAddr + " @ " + _ipAddr.toString());
 }
 
 // Get state of WiFi connection
 SensorNet::WIFI_STATE SensorNet::wifiState() {
-	SensorNet::WIFI_STATE status = {
-		WiFi.status(),
-		_macAddr,
-		_ipAddr,
-		WiFi.RSSI()
-	};
-	return status;
+    SensorNet::WIFI_STATE status = {
+        WiFi.status(),
+        _macAddr,
+        _ipAddr,
+        WiFi.RSSI()
+    };
+    return status;
 }
 
 // Setup connection to MQTT server and make initial connection
 void SensorNet::mqttSetup(String server, int port, String prefix) {
-	//// TODO assert that WiFi.status() == WL_CONNECTED
-	server.toCharArray(mqttServer, BUF_SIZE);
-	mqttPort = port;
-  mqttClient.setServer(mqttServer, mqttPort);
-//  mqttClient.setSocketTimeout(?);
-//  mqttClient.setKeepalive(?);
+    //// TODO assert that WiFi.status() == WL_CONNECTED
+    server.toCharArray(mqttServer, BUF_SIZE);
+    mqttPort = port;
+    mqttClient.setServer(mqttServer, mqttPort);
+//    mqttClient.setSocketTimeout(?);
+//    mqttClient.setKeepalive(?);
 
-  String topic = prefix + "/" + _macAddr + "/cmd";
-  topic.toCharArray(cmdTopic, MAX_MQTT_TOPIC_LEN);
+    String topic = prefix + "/" + _macAddr + "/cmd";
+    topic.toCharArray(cmdTopic, MAX_MQTT_TOPIC_LEN);
 
-  topic = prefix + "/" + _macAddr + "/data";
-  topic.toCharArray(dataTopic, MAX_MQTT_TOPIC_LEN);
+    topic = prefix + "/" + _macAddr + "/data";
+    topic.toCharArray(dataTopic, MAX_MQTT_TOPIC_LEN);
 
-  mqttRun();
-  String startupMsg = "Startup,ESP8266," + appName + "," + appVersion + "," + reportSchema + "," + wifiState().rssi;
-  startupMsg.toCharArray(pubMsg, MAX_MQTT_PUB_MSG_LEN);
-  mqttClient.publish(cmdTopic, pubMsg);
+    mqttRun();
+    String startupMsg = "Startup,ESP8266," +
+                        appName + "," +
+                        appVersion + "," +
+                        reportSchema + "," +
+                        wifiState().rssi;
+    startupMsg.toCharArray(pubMsg, MAX_MQTT_PUB_MSG_LEN);
+    mqttClient.publish(cmdTopic, pubMsg);
 }
 
 // Connect to the MQTT server
 void SensorNet::mqttRun() {
-	//// TODO assert that MQTT has been set up
-  while (!mqttClient.connected()) {
-    consolePrint("Connecting to MQTT...");
-    if (mqttClient.connect(_clientName)) {
-      consolePrintln("connected");
-    } else {
-      consolePrintln("ERROR: failed with state " + mqttClient.state());
-      delay(2000);
+    //// TODO assert that MQTT has been set up
+    while (!mqttClient.connected()) {
+        consolePrint("Connecting to MQTT...");
+        if (mqttClient.connect(_clientName)) {
+            consolePrintln("connected");
+        } else {
+            consolePrintln("ERROR: failed with state " + mqttClient.state());
+            delay(2000);
+        }
     }
-  }
-  mqttClient.loop();
+    mqttClient.loop();
 }
 
 // Publish message to the defined topic
 void SensorNet::mqttPub(String msg) {
-	msg.toCharArray(pubMsg, MAX_MQTT_PUB_MSG_LEN);
-	mqttClient.publish(dataTopic, pubMsg);
-//	consolePrintln("ZZZZ");
-//	mqttClient.publish(dataTopic, "YYYY");
+    msg.toCharArray(pubMsg, MAX_MQTT_PUB_MSG_LEN);
+    mqttClient.publish(dataTopic, pubMsg);
 }
 
-// Subscribe to a topic and give a callback handler
-void SensorNet::mqttSub(void (*callback)(char *, byte *, unsigned int)) {
-  mqttClient.setCallback(callback);
-  if (mqttClient.subscribe(cmdTopic) == false) {
-  	consolePrint("ERROR: failed to subscribe to topic -- ");
-  	consolePrintln(cmdTopic);
-  	//// TODO handle error
-  } else {
-	  consolePrintln(String("Subscribed to: ") + String(cmdTopic));
-	}
+// Add a callback function to the subscribed "cmd" topic
+void SensorNet::mqttSub(void (*callback)(char *topic, byte *payload, unsigned int length) {
+    mqttClient.setCallback(_callback);
+    if (mqttClient.subscribe(cmdTopic) == false) {
+         consolePrint("ERROR: failed to subscribe to topic -- ");
+         consolePrintln(cmdTopic);
+         //// TODO handle error
+    } else {
+        consolePrintln(String("Subscribed to: ") + String(cmdTopic));
+    }
 }
 
 SensorNet::MQTT_STATE SensorNet::mqttState() {
-	SensorNet::MQTT_STATE state = {
-		String(mqttServer),
-		mqttPort,
-		String(dataTopic),
-		String(cmdTopic)
-	};
-	return state;
+    SensorNet::MQTT_STATE state = {
+        String(mqttServer),
+        mqttPort,
+        String(dataTopic),
+        String(cmdTopic)
+    };
+    return state;
 }
--- libraries/SensorNet/src/SensorNet.h
+++ libraries/SensorNet/src/SensorNet.h
@@ -8,67 +8,69 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define NUM_SERIALS      	 	2
-#define MAX_MQTT_PUB_MSG_LEN	180
-#define MAX_MQTT_TOPIC_LEN		128
-#define BUF_SIZE              	64
+#define NUM_SERIALS               2
+#define MAX_MQTT_PUB_MSG_LEN    180
+#define MAX_MQTT_TOPIC_LEN        128
+#define BUF_SIZE                  64
 
 #define ELEMENTS(x)   (sizeof(x) / sizeof(x[0]))
 
 class SensorNet {
-	public:
-		String appName = "n/a";
-		String appVersion = "n/a";
-		String reportSchema = "n/a";
-		HardwareSerial *consolePtr;
+    public:
+        String appName = "n/a";
+        String appVersion = "n/a";
+        String reportSchema = "n/a";
+        HardwareSerial *consolePtr;
 
-		struct WIFI_STATE {
-		  wl_status_t state;
-		  String macAddr;
-		  IPAddress ipAddr;
-		  long rssi;
-		};
+        struct WIFI_STATE {
+            wl_status_t state;
+            String macAddr;
+            IPAddress ipAddr;
+          long rssi;
+        };
 
-		struct MQTT_STATE {
-		  String server;
-		  int port;
-		  String dataTopic;
-		  String cmdTopic;
-		};
+        struct MQTT_STATE {
+            String server;
+            int port;
+            String dataTopic;
+            String cmdTopic;
+        };
 
-	  SensorNet();
-	  SensorNet(String name);
-	  SensorNet(String name, String version);
-	  SensorNet(String name, String version, String schema);
+      SensorNet();
+      SensorNet(String name);
+      SensorNet(String name, String version);
+      SensorNet(String name, String version, String schema);
 
-	  void serialStart(HardwareSerial *portPtr, uint16 baud, bool console);
-	  void consolePrint(String str);
-	  void consolePrintln(String str);
-	  void consoleWaitForInput();
+      void serialStart(HardwareSerial *portPtr, uint16 baud, bool console);
+      void consolePrint(String str);
+      void consolePrintln(String str);
+      void consoleWaitForInput();
 
-    void wifiStart(String ssid, String password);
-	  WIFI_STATE wifiState();
+      void wifiStart(String ssid, String password);
+      WIFI_STATE wifiState();
 
-	  void mqttSetup(String server, int port, String prefix);
-	  void mqttRun();
-	  void mqttPub(String msg);
-	  void mqttSub(void (*callback)(char *, byte *, unsigned int));
-	  MQTT_STATE mqttState();
+      void mqttSetup(String server, int port, String prefix);
+      void mqttRun();
+      void mqttPub(String msg);
+      void mqttSub(void (*callback)(String topic, String msg));
+      MQTT_STATE mqttState();
 
-	private:
-  	byte _mac[6];
-  	String _macAddr;
-  	IPAddress _ipAddr;
-	char _clientName[BUF_SIZE];
+    private:
+        byte _mac[6];
+        String _macAddr;
+        IPAddress _ipAddr;
+        char _clientName[BUF_SIZE];
 
-  	char mqttServer[BUF_SIZE];
-	int mqttPort;
-    char dataTopic[MAX_MQTT_TOPIC_LEN];
-    char cmdTopic[MAX_MQTT_TOPIC_LEN];
-	char pubMsg[MAX_MQTT_PUB_MSG_LEN];
+        char mqttServer[BUF_SIZE];
+        int mqttPort;
+        char dataTopic[MAX_MQTT_TOPIC_LEN];
+        char cmdTopic[MAX_MQTT_TOPIC_LEN];
+        char pubMsg[MAX_MQTT_PUB_MSG_LEN];
 
- 	WiFiClient espClient;
-  	PubSubClient mqttClient = PubSubClient(espClient);
+        WiFiClient espClient;
+        PubSubClient mqttClient = PubSubClient(espClient);
+
+		void _callback(char *topic, byte *payload, unsigned int length);
 };
 
 #endif /*SENSOR_NET_H*/

1579ef23eb31f65d49faf3c58c324dda96268e73
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-09-03 15:45:24 -0700

    Created.

---
+++ tools/monitorScripts/genericMissed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Generic script to run when samples from a device are missing
+#
+
+echo "${} missing since ${}"
---
+++ tools/monitorScripts/genericSeen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Generic script to run when a sample is seen from a device
+#
+
+echo "${} Seen at ${} with ${}"

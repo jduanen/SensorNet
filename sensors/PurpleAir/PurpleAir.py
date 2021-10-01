53a59cf2096ac43c9e056c6f3b4fef0dcb6e2feb
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-09-30 20:33:38 -0700

    Enabled publishing of MQTT msgs. Added stub for back-channel commands.

--- sensors/PurpleAir/PurpleAir.py
+++ sensors/PurpleAir/PurpleAir.py
@@ -7,11 +7,15 @@ PurpleAir -- tool for polling Purple Air sensors as SensorNet sensors
 '''
 
 import argparse
+import ast
 import json
 import logging
 import os
 import random
+import re
 import signal
+import socket
+import subprocess
 import sys
 import threading
 import time
@@ -19,8 +23,9 @@ import yaml
 from yaml import Loader
 
 import paho.mqtt.client as mqtt
+from purpleair.sensor import Sensor
 
-#from SensorNet import SensorNet, SubTopic, SUB_TOPICS
+from SensorNet import SensorNet, SubTopic, SUB_TOPICS
 
 
 DEFAULTS = {
@@ -31,34 +36,75 @@ DEFAULTS = {
 
 DEF_SAMPLE_INTERVAL = 120 # 2mins between samples
 
-MQTT_TOPIC_BASE = "/sensors/#"
+MQTT_TOPIC_BASE = "/sensors/PurpleAir"
+
+APP_VERSION = "1.0.0"
+
+
+def getMacAddress(hostname):
+    """Return the MAC address for the interface used to reach the given host
+
+      Inputs:
+        hostname: string name of the target host
+      Returns: string form of MAC address of interface used to reach given host
+    """
+    ipAddr = socket.gethostbyname(hostname)
+    match = re.search(r"^.* dev (.*?) .*$", str(subprocess.check_output(["ip", "route", "get", ipAddr])))
+    assert match, f"Unable to find interface for {ipAddr}"
+    intf = match.group(1)
+    return subprocess.check_output(["cat", f"/sys/class/net/{intf}/address"]).strip().decode("utf-8")
 
 
 class PurpleAir():
     """????
     """
     def __init__(self, sensors, mqttBroker, sampleInterval=DEF_SAMPLE_INTERVAL, retries=3):
-        self.sensors = sensors
+        self.sensorsInput = sensors
         self.mqttBroker = mqttBroker
         self.sampleInterval = sampleInterval
         self.retries = retries
+
+        self.sensorIds = list(sensors.keys())
+        self.sensors = {sensorId: Sensor(sensorId, parse_location=True) for sensorId in self.sensorIds}
+        for sensorId, sensor in self.sensors.items():
+            if sensor.location_type != 'outside':
+                logging.warning(f"Sensor '{sensorId}' not outside, removing this sensor from the list")
+                del self.sensors[sensorId]
         self.secureRandom = random.SystemRandom()
+
+        self.baseMsg = f"{MQTT_TOPIC_BASE}/{getMacAddress(self.mqttBroker)}"
+        self.cmdTopic = f"{self.baseMsg}/{SUB_TOPICS[SubTopic.COMMAND]}"
+        self.dataTopic = f"{self.baseMsg}/{SUB_TOPICS[SubTopic.DATA]}"
+        RSSI = 0
+        msgSpec = f"ID:int,Label:s,tempF:d3,pressure:f4.2,humidity:d3," + \
+                  f"v:f3.2,v1:f3.2,v2:f3.2,v3:f3.2,v4:f3.2,v5:f3.2,v6:f3.2," + \
+                  f"pm:f3.2,lastModified:d13,timeSinceModified:d"
+        self.startupMsg = f"Startup,{socket.gethostname()},PurpleAir,{APP_VERSION},{msgSpec},{RSSI}"
 
         self.client = mqtt.Client("PurpleAir sensor proxy")
         self.client.enable_logger(logging.getLogger(__name__))
         if self.client.connect(self.mqttBroker):
             logging.error(f"Failed to connect to MQTT broker '{self.mqttBroker}'")
             raise Exception("Failed to connect to MQTT broker")
-
-        #### TODO implement listener for backchannel controls
-        '''
+        result, msgId = self.client.subscribe(f"{self.cmdTopic}")
+        if result:
+            logging.error(f"Failed to subscribe to topic '{self.cmdTopic}'")
+            raise Exception("Subscription failure")
         self.client.on_message = self._onMessage
         self.client.loop_start()
-        '''
 
         self.running = threading.Event()
         self.running.clear()
         self.thread = threading.Thread(target=self.run, name="PurpleAir")
+
+    def _onMessage(self, client, userData, message):
+        parts = message.topic.split('/')
+        print(f"MSG: {parts}")
+        if len(parts) < 5 or len(parts) > 6 or parts[1] != 'sensors' or parts[-1] == SubTopic.COMMAND:
+            logging.warning(f"Unrecognized message: {message}")
+        msg = message.payload.decode("utf-8")
+        #### TODO implement the command handler here
+        print("TBD")
 
     def start(self):
         """????
@@ -85,14 +131,27 @@ class PurpleAir():
     def run(self):
         """????
         """
-        #### TODO publish startup message
+        self.client.publish(self.cmdTopic, payload=self.startupMsg)
+        logging.info(self.startupMsg)
+        RSSI = 0
         while self.running.isSet():
             tryNumber = 0
             while True:
-                sensorId = self.secureRandom.choice(list(self.sensors.keys()))
+                sensorId = self.secureRandom.choice(self.sensorIds)
                 logging.debug(f"Sampling sensor id #{sensorId}")
-                #### TODO read sensor, and publish it
-                #### TODO if hasn't been updated recently, try another one -- up to a limit
+                sensor = self.sensors[sensorId]
+                if sensor.is_useful():
+                    data = sensor.parent_data
+                    stats = ast.literal_eval(data['Stats'])
+                    assert stats['timeSinceModified'] < 180000, f"Sensor {sensorId} not seen for {stats['timeSinceModified']}"
+                    msg = f"{data['ID']},{data['Label']},{data['temp_f']},{data['pressure']},{data['humidity']},"
+                    msg += ",".join([str(s) for s in stats.values()]) + f",{RSSI}"
+                    #### TODO publish sensor data
+                    self.client.publish(self.dataTopic, payload=msg)
+                    logging.info(msg)
+                    break
+                else:
+                    logging.warning(f"Sensor '{sensorId}' is not useful, skipping")
                 tryNumber += 1
                 if tryNumber >= self.retries:
                     logging.warning(f"Failed to get good sample in {tryNumber} attempts, giving up")
@@ -115,12 +174,6 @@ class PurpleAir():
     '''
 
     '''
-    def _onMessage(self, client, userData, message):
-        parts = message.topic.split('/')
-        if len(parts) < 5 or len(parts) > 6 or parts[1] != 'sensors' or parts[-1] not in SUB_TOPICS.values():
-            logging.warning(f"Unrecognized message: {message}")
-        topic = "/".join(parts)
-        self.msgQ.put((topic, message.payload.decode("utf-8"), message.timestamp))
 
     def __enter__(self):
         return self

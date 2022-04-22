007baa31ee571ba221f8e3d3ed6d4469e02fe276
Duane Northcutt <duane.northcutt@gmail.com>
2022-04-21 19:03:25 -0700

    Fixed indentation. Made local names consistent.

--- libraries/SensorNet/src/SensorNet.h
+++ libraries/SensorNet/src/SensorNet.h
@@ -5,23 +5,23 @@
 #ifndef SENSOR_NET_H
 #define SENSOR_NET_H
 
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
 #include <PubSubClient.h>
+#include <ESP8266WiFi.h>
 
 
 #define NUM_SERIALS             2
 #define MAX_MQTT_PUB_MSG_LEN    180
 #define MAX_MQTT_TOPIC_LEN      128
 #define BUF_SIZE                64
-#define NUM_SUB_TOPICS			5
+#define NUM_SUB_TOPICS          5
 #define DEF_REPORT_INTERVAL     60000  // one report every minute
-#define LIB_VERSION             "1.2"
+#define LIB_VERSION             "2.0"
 
 #define ELEMENTS(x)   (sizeof(x) / sizeof(x[0]))
 
 
 typedef void (callback)(char *topic, byte *payload, unsigned int length);
-
 
 class SensorNet {
     public:
@@ -36,11 +36,11 @@ class SensorNet {
 
         // pub msg types -- 1:1 correspondance to sub-topics
         typedef char pubType;
-		static const pubType DATA = 0;
-		static const pubType COMMAND = 1;
-		static const pubType RESPONSE = 2;
-		static const pubType ERROR = 3;
-		static const pubType STARTUP = 4;
+        static const pubType DATA = 0;
+        static const pubType COMMAND = 1;
+        static const pubType RESPONSE = 2;
+        static const pubType ERROR = 3;
+        static const pubType STARTUP = 4;
 
         struct WIFI_STATE {
             wl_status_t state;
@@ -66,7 +66,7 @@ class SensorNet {
         SensorNet(String name, String version);
         SensorNet(String name, String version, String schema);
 
-	    void(* systemReset)(void) = 0;
+        void(* systemReset)(void) = 0;
 
         void serialStart(HardwareSerial *portPtr, uint16 baud, bool console);
         void consolePrint(String str);
@@ -90,17 +90,17 @@ class SensorNet {
         IPAddress _ipAddr;
         char _clientName[BUF_SIZE];
 
-        char mqttServer[BUF_SIZE];
-        int mqttPort;
-        String baseTopic;
-        char pubMsg[MAX_MQTT_PUB_MSG_LEN];
+        char _mqttServer[BUF_SIZE];
+        int _mqttPort;
+        String _baseTopic;
+        char _pubMsg[MAX_MQTT_PUB_MSG_LEN];
 
-        WiFiClient espClient;
-        PubSubClient mqttClient = PubSubClient(espClient);
+        WiFiClient _espClient;
+        PubSubClient _mqttClient = PubSubClient(_espClient);  //// FIXME dynamically allocate this
 
-        char topics[NUM_SUB_TOPICS][MAX_MQTT_TOPIC_LEN];
+        char _topics[NUM_SUB_TOPICS][MAX_MQTT_TOPIC_LEN];
 
-        void topicSubscribe(String topic);
+        void _topicSubscribe(String topic);
 };
 
 #endif /*SENSOR_NET_H*/

b1a5f039b141120e78a63a8d01859c5749fc841f
Duane Northcutt <duane.northcutt@gmail.com>
2022-05-14 13:21:57 -0700

    Added test options.

--- libraries/WebServices/examples/WebServicesTest/WebServicesTest.ino
+++ libraries/WebServices/examples/WebServicesTest/WebServicesTest.ino
@@ -17,12 +17,28 @@
 //#define USER_NAME       "name"
 //#define PASSWD          "passwd"
 
+#define CONFIG_PATH         "/config.json"
+#define ROOT_PAGE_PATH      "/"
+
+#define FW_UPDATE           (1 << 0)
+#define CONFIG_FILE         (1 << 1)
+#define COMMON_GUI          (1 << 2)
+#define APPL_GUI            (1 << 3)
+
+//#define TEST_MODE           FW_UPDATE
+//#define TEST_MODE           FW_UPDATE | CONFIG_FILE
+//#define TEST_MODE           FW_UPDATE | COMMON_GUI
+//#define TEST_MODE           FW_UPDATE | APPL_GUI
+//#define TEST_MODE           FW_UPDATE | CONFIG_FILE | COMMON_GUI
+//#define TEST_MODE           FW_UPDATE | CONFIG_FILE | COMMON_GUI | APPL_GUI
+
 
 const char* ssid = WLAN_SSID;
 const char* password = WLAN_PASS;
 
 WebServices webSvcs(WEB_SERVER_PORT);
 
+int testMode = FW_UPDATE | COMMON_GUI;
 int loopCnt = 0;
 
 void setup() {
@@ -39,10 +55,20 @@ void setup() {
 
   Serial.println(WiFi.localIP());
 
-  webSvcs.setup("", "");
-//  webSvcs.setup("/config.json", null);
-//  webSvcs.setup(null, "/");
-//  webSvcs.setup("/config.json", "/");
+  String confPath = "";
+  String rootPagePath = "";
+
+  if ((testMode & CONFIG_FILE) == CONFIG_FILE) {
+    confPath = CONFIG_PATH;
+  }
+  if ((testMode & COMMON_GUI) == COMMON_GUI) {
+    rootPagePath = ROOT_PAGE_PATH;
+  }
+  if ((testMode & APPL_GUI) == APPL_GUI) {
+    Serial.println("ERROR: TBD");
+  }
+
+  webSvcs.setup(confPath, rootPagePath);
 }
 
 void loop() {

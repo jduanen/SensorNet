c45ebb4d257c1c0a7e022d3060d7c46013da1003
Duane Northcutt <duane.northcutt@gmail.com>
2022-05-16 19:26:54 -0700

    Added check for invalid wifi state save. Added code to onMessage().

--- libraries/WebServices/src/data/common/wsScripts.js
+++ libraries/WebServices/src/data/common/wsScripts.js
@@ -1,19 +1,19 @@
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
-window.addEventListener('load', onLoad);
+window.addEventListener("load", onLoad);
 function initWebSocket() {
-  console.log('Trying to open a WebSocket connection...');
+  console.log("Trying to open a WebSocket connection...");
   websocket = new WebSocket(gateway);
   websocket.onopen    = onOpen;
   websocket.onclose   = onClose;
   websocket.onmessage = onMessage;
 }
 function onOpen(event) {
-  console.log('Connection opened');
+  console.log("Connection opened");
   initView();
 }
 function onClose(event) {
-  console.log('Connection closed');
+  console.log("Connection closed");
   setTimeout(initWebSocket, 2000);
 }
 function onLoad(event) {
@@ -25,15 +25,34 @@ function initView() {
   console.log("FIXME: initView");
 }
 function onMessage(event) {
-  var state;
-  var elem;
+  console.log("msgObj: " + event.data);
   const msgObj = JSON.parse(event.data);
-  console.log('msgObj: ', msgObj);
+  document.getElementById("libVersion").textContent = escapeHTML(msgObj.libVersion);
+  document.getElementById("ipAddr").textContent = escapeHTML(msgObj.ipAddr);
+  document.getElementById("connected").textContent = escapeHTML(msgObj.connected);
+  document.getElementById("rssi").textContent = escapeHTML(msgObj.RSSI);
 }
 function saveConfiguration() {
   var ssid = document.getElementById("ssid").value;
+  if ((typeof ssid == "undefined") || (ssid == "")) {
+    console.log("Undefined SSID, not saving");
+    return;
+  }
   var passwd = document.getElementById("password").value;
+  if ((typeof passwd == "undefined") || (passwd == "")) {
+    console.log("Undefined password, not saving");
+    return;
+  }
+  //// TODO do some simple obfuscation of the password -- e.g., rot13 so at least it's not in the clear
   var jsonMsg = JSON.stringify({"msgType": "saveConf", "ssid": ssid, "passwd": passwd});
-  console.log("Save configuration: " + jsonMsg);
+  console.log("Save configuration: " + jsonMsg);  //// TMP TMP TMP
   websocket.send(jsonMsg);
+}
+function escapeHTML(s) {
+  return s.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/'/g, '&apos;')
+      .replace(/"/g, '&quot;')
+      .replace(/\//g, '&sol;');
 }

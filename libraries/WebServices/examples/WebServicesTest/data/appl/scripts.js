6999230aa202d7377d424389a895fc7bd6b8f995
Duane Northcutt <duane.northcutt@gmail.com>
2022-05-16 19:25:30 -0700

    Created.

---
+++ libraries/WebServices/examples/WebServicesTest/data/appl/scripts.js
@@ -0,0 +1,11 @@
+function initApplView() {
+  console.log("FIXME: initApplView");
+}
+function onApplMessage(event) {
+  const msgObj = JSON.parse(event.data);
+  console.log("FIXME onApplMessage");
+  console.log("msgObj=", msgObj);
+}
+function saveApplConfiguration() {
+  console.log("FIXME saveApplConfiguration: ");
+}

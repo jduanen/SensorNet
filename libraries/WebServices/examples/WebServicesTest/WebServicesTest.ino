91f92d6a4a5ed125a20b098590e6776f4da6f3cd
Duane Northcutt <duane.northcutt@gmail.com>
2022-06-23 19:29:43 -0700

    Changed to four spaces.

--- libraries/WebServices/examples/WebServicesTest/WebServicesTest.ino
+++ libraries/WebServices/examples/WebServicesTest/WebServicesTest.ino
@@ -7,7 +7,7 @@
  *
  ***************************************************************************/
 
-#include <WebServices.h>
+#include "WebServices.h"
 #include "wifi.h"
 
 
@@ -16,24 +16,17 @@
 #define APPL_NAME           "WebServicesTest"
 #define APPL_VERSION        "1.0.0"
 #define WEB_SERVER_PORT     80
-//#define USER_NAME       "name"
-//#define PASSWD          "passwd"
+//#define USER_NAME           "name"
+//#define PASSWD              "passwd"
 
 #define CONFIG_PATH         "/config.json"
 #define COMMON_PAGE_PATH    "/"
-#define APPL_PAGE_PATH      "/app"
+#define APPL_PAGE_PATH      "/appl"
 
 #define FW_UPDATE           (1 << 0)
 #define CONFIG_FILE         (1 << 1)
 #define COMMON_GUI          (1 << 2)
 #define APPL_GUI            (1 << 3)
-
-//#define TEST_MODE           FW_UPDATE
-//#define TEST_MODE           FW_UPDATE | CONFIG_FILE
-//#define TEST_MODE           FW_UPDATE | COMMON_GUI
-//#define TEST_MODE           FW_UPDATE | APPL_GUI
-//#define TEST_MODE           FW_UPDATE | CONFIG_FILE | COMMON_GUI
-//#define TEST_MODE           FW_UPDATE | CONFIG_FILE | COMMON_GUI | APPL_GUI
 
 
 const char* ssid = WLAN_SSID;
@@ -41,40 +34,47 @@ const char* password = WLAN_PASS;
 
 WebServices webSvcs(APPL_NAME, WEB_SERVER_PORT);
 
-int testMode = FW_UPDATE | COMMON_GUI;
+int testMode = FW_UPDATE | COMMON_GUI | APPL_GUI;
+
 int loopCnt = 0;
 
 
+String commonHandlerWrapper(StaticJsonDocument<WS_JSON_DOC_SIZE> wsMsg) {
+    return(webSvcs.commonMsgHandler(wsMsg));
+}
+
+
 void setup() {
-  delay(500);
-  Serial.begin(19200);
-  delay(500);
-  Serial.println("\nBEGIN");
+    delay(500);
+    Serial.begin(19200);
+    delay(500);
+    Serial.println("\nBEGIN");
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi..");
+    }
 
-  Serial.println(WiFi.localIP());
+    Serial.println(WiFi.localIP());
 
-  String confPath = "";
-  String commonPagePath = "";
-  String applPagePath = "";
+    String confPath = "";
+    String commonPagePath = "";
+    String applPagePath = "";
 
-  if ((testMode & CONFIG_FILE) == CONFIG_FILE) {
-    confPath = CONFIG_PATH;
-  }
-  if ((testMode & COMMON_GUI) == COMMON_GUI) {
-    commonPagePath = COMMON_PAGE_PATH;
-  }
-  if ((testMode & APPL_GUI) == APPL_GUI) {
-    applPagePath = APPL_PAGE_PATH;
-  }
+    if ((testMode & CONFIG_FILE) == CONFIG_FILE) {
+        confPath = CONFIG_PATH;
+    }
+    if ((testMode & COMMON_GUI) == COMMON_GUI) {
+        commonPagePath = COMMON_PAGE_PATH;
+    }
+    if ((testMode & APPL_GUI) == APPL_GUI) {
+        applPagePath = APPL_PAGE_PATH;
+    }
 
-  //// TODO register appl page
-  webSvcs.setup(confPath, commonPagePath, applPagePath);
+    //// TODO register all pages
+    //  (confPath, commonPagePath, applPagePath);
+    webSvcs.addPage(commonPagePath, COMMON_STYLE_PATH, COMMON_SCRIPT_PATH);
 }
 
 void loop() {

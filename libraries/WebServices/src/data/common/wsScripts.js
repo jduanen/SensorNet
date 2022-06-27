4ba985b316e1a98d51e4be6666a70d5945cf09cc
Duane Northcutt <duane.northcutt@gmail.com>
2022-06-27 16:07:24 -0700

    Fixed Wifi mode status.

--- libraries/WebServices/src/data/common/wsScripts.js
+++ libraries/WebServices/src/data/common/wsScripts.js
@@ -23,16 +23,26 @@ function initView() {
   var jsonMsg = JSON.stringify({"msgType": "query"});
   websocket.send(jsonMsg);
   var wifiMode = "%WIFI_MODE%";
+  var elem;
   switch (wifiMode) {
     case "STA":
-      var p1 = "<p class=\"staticState\">IP Address: <span style=\"color:blue\" id=\"ipAddr\">%IP_ADDR%</span></p>";
-      var p2 = "<p class=\"staticState\">Connection: <span style=\"color:blue\" id=\"connection\">%CONNECTION%</span></p>";
-      document.getElementById("wifiMode").insertAdjacentHTML('afterend', p1);
-      document.getElementById("ipAddr").insertAdjacentHTML('afterend', p2);
+      elem = document.getElementById("ipAddr");
+      if (elem == null) {
+        var p1 = "<p class=\"staticState\">IP Address: <span style=\"color:blue\" id=\"ipAddr\">%IP_ADDR%</span></p>";
+        document.getElementById("wifiMode").insertAdjacentHTML('afterend', p1);
+      }
+      elem = document.getElementById("connection");
+      if (elem == null) {
+        var p2 = "<p class=\"staticState\">Connection: <span style=\"color:blue\" id=\"connection\">%CONNECTION%</span></p>";
+        document.getElementById("ipAddr").insertAdjacentHTML('afterend', p2);
+      }
       break;
     case "A_P":
-      var p1 = "<p class=\"staticState\">AP SSID: <span style=\"color:blue\" id=\"wifiApSsid\">%WIFI_AP_SSID%</span></p>";
-      document.getElementById("wifiMode").insertAdjacentHTML('afterend', p1);
+      elem = document.getElementById("wifiApSsid");
+      if (elem == null) {
+        var p1 = "<p class=\"staticState\">AP SSID: <span style=\"color:blue\" id=\"wifiApSsid\">%WIFI_AP_SSID%</span></p>";
+        document.getElementById("wifiMode").insertAdjacentHTML('afterend', p1);
+      }
       break;
     default:
       console.log("WARNING: invalid WIFI mode: " + wifiMode);

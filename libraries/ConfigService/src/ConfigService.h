3c4050d2371ac7a1f65139e65e451fa8a3274919
Duane Northcutt <duane.northcutt@gmail.com>
2022-12-02 10:08:54 -0800

    Changed doc to docPtr. Changed methods to reflect new model.

--- libraries/ConfigService/src/ConfigService.h
+++ libraries/ConfigService/src/ConfigService.h
@@ -14,41 +14,60 @@
 #include "LfsUtilities.h"
 
 
-#define CONFIG_SERVICE_VERSION  "2.0"
+#define CONFIG_SERVICE_VERSION  "2.1"
 
 #define MAX_PATH_LENGTH         32
 
-#define INIT_CONFIG_PTR(csPtr, field, value)        if (!csPtr->validEntry(field)) {(*(csPtr->doc))[field] = value;}
-#define SET_CONFIG_PTR(csPtr, field, value)         (*(csPtr->doc))[field] = value
-#define GET_CONFIG_PTR(value, csPtr, field, typ)    value = (*(csPtr->doc))[field].as<typ>()
+/*
+#define INIT_CONFIG_PTR(csPtr, field, value)        if (!csPtr->validEntry(field)) {(*(csPtr->docPtr))[field] = value;}
+#define SET_CONFIG_PTR(csPtr, field, value)         (*(csPtr->docPtr))[field] = value
+#define GET_CONFIG_PTR(value, csPtr, field, typ)    value = (*(csPtr->docPtr))[field].as<typ>()
 
-#define INIT_CONFIG(csObj, field, value)        if (!csObj.validEntry(field)) {(*(csObj.doc))[field] = value;}
-#define SET_CONFIG(csObj, field, value)         (*(csObj.doc))[field] = value
-#define GET_CONFIG(value, csObj, field, typ)    value = (*(csObj.doc))[field].as<typ>()
+#define INIT_CONFIG(csObj, field, value)            if (!csObj.validEntry(field)) {(*(csObj.docPtr))[field] = value;}
+#define SET_CONFIG(csObj, field, value)             (*(csObj.docPtr))[field] = value
+*/
+#define CS_DOC(cs)                              (*(cs.docPtr))
+#define CSPTR_DOC(csPtr)                        (*(csPtr->docPtr))
+#define GET_CONFIG(csPtr, field, value, typ)    value = (*(csPtr->docPtr))[field].as<typ>()
+//#define INIT_STATE(csPtr, field, value, typ)    if (csPtr->validEntry(field)) {GET_CONFIG(csPtr, field, value, typ);}
+#define INIT_STATE(csPtr, field, value, typ)    Serial.print(field); if (csPtr->validEntry(field)) {Serial.println(" from file"); GET_CONFIG(csPtr, field, value, typ);} else {Serial.println(" from defaults");}
+
+//// FIXME -- types: string, char, int/float, bool, tuples
+/*
+#define ADD_STRING_FIELD(jsonStr, field, value) jsonStr.concat(field: value)
+#define ADD_INT_FIELD(jsonStr, field, value)    jsonStr.concat(field: value)
+#define ADD_CHAR_FIELD(jsonStr, field, value)   jsonStr.concat(field: value)
+#define ADD_BOOL_FIELD(jsonStr, field, value)   jsonStr.concat(field: value)
+#define ADD_FLOAT_FIELD(jsonStr, field, value)  jsonStr.concat(field: value)
+*/
 
 
 class ConfigService {
 public:
     String libVersion = CONFIG_SERVICE_VERSION;
-    DynamicJsonDocument *doc;
+    DynamicJsonDocument *docPtr;
 
     ConfigService(uint32_t docSize, const String& configFilePath);
     ~ConfigService();
     static ConfigService &getInstance();
 
-    void format();
+    void formatFS();
     void listFiles(const String& path);
+
+    bool validEntry(const String& key);
+    uint32_t configSize();
+
+    bool setConfig(const String& json);
     bool initializeConfig();
     bool saveConfig();
-    void printConfig();
-    bool deleteConfig();
-    bool validEntry(const String& key);
+    void printConfigFile();
+    bool deleteConfigFile();
 
 private:
     bool _verbose = true;
     char _configPath[MAX_PATH_LENGTH];
 
-    bool _readConfig();
+    bool _readConfigFile();
     void _printConfigDoc();
 
     void _print(String str);
--- libraries/ConfigService/src/ConfigService.hpp
+++ libraries/ConfigService/src/ConfigService.hpp
@@ -5,7 +5,13 @@
  ***************************************************************************/
 
 ConfigService::ConfigService(uint32_t docSize, const String& configFilePath) {
-    doc = new DynamicJsonDocument(docSize);
+    int maxFreeBlkSize = ESP.getMaxFreeBlockSize();
+    if (maxFreeBlkSize < docSize) {
+        Serial.println("ERROR: insufficiently large free block -- (" + String(maxFreeBlkSize) + ", " + String(docSize) + ")");
+        //// FIXME throw exception on failure
+        return;
+    }
+    docPtr = new DynamicJsonDocument(docSize);
     if (!lfs.mountLFS()) {
         Serial.println("ERROR: failed to mount LittleFS");
         //// FIXME throw exception on failure
@@ -15,36 +21,51 @@ ConfigService::ConfigService(uint32_t docSize, const String& configFilePath) {
     if (!LittleFS.exists(_configPath)) {
         _println("WARNING: No config file; initializing config file with an empty JSON object");
         if (!initializeConfig()) {
+            Serial.println("ERROR: unable to initialize empty config file");
+            //// FIXME throw an exception
             return;
         }
     }
-    if (!_readConfig()) {
-        Serial.println("ERROR: failed to read file: ");
-        Serial.println(_configPath);
+    if (!_readConfigFile()) {
+        Serial.print("ERROR: failed to read file: "); Serial.println(_configPath);
+        //// FIXME throw an exception
         return;
     }
-}
+};
 
 ConfigService::~ConfigService() {
     lfs.unmountLFS();
-}
+};
 
-void ConfigService::format() {
+void ConfigService::formatFS() {
     lfs.formatLFS();
-}
+};
 
 void ConfigService::listFiles(const String& path) {
     lfs.listFilesLong(path);
-}
+};
 
-bool ConfigService::initializeConfig() {
-    DeserializationError error = deserializeJson(*doc, "{}");
+bool ConfigService::validEntry(const String& key) {
+    return (docPtr->containsKey(key) && !(*docPtr)[key].isNull());
+};
+
+uint32_t ConfigService::configSize() {
+    return(docPtr->memoryUsage());
+};
+
+bool ConfigService::setConfig(const String& json) {
+    DeserializationError error = deserializeJson(*docPtr, json);
     if (error) {
-        Serial.print("ERROR: initializeConfig() deserializeJson() failed: ");
+        Serial.print("ERROR: setConfig() deserializeJson() failed: ");
         Serial.println(error.f_str());
+        return(false);
     };
     return(saveConfig());
-}
+};
+
+bool ConfigService::initializeConfig() {
+    return(setConfig("{}"));
+};
 
 bool ConfigService::saveConfig() {
     File f = LittleFS.open(_configPath, "w");
@@ -52,64 +73,64 @@ bool ConfigService::saveConfig() {
         Serial.printf("ERROR: failed to write config file: %s\n", _configPath);
         return(false);
     }
-    serializeJson(*doc, f);
+    size_t bytesWritten = serializeJson(*docPtr, f);
     if (_verbose) {
-        Serial.printf("Wrote serialized JSON to config file: %s\n", _configPath);
-        printConfig();
+        Serial.printf("Wrote serialized JSON to config file: %s (%d Bytes)\n", _configPath, bytesWritten);
+        printConfigFile();
         Serial.println("");
     }
     f.close();
     return(true);
-}
+};
 
-void ConfigService::printConfig() {
+void ConfigService::printConfigFile() {
     _println(String("Read and print config file: ") + _configPath);
     lfs.printFile(_configPath);
-}
+};
 
-bool ConfigService::deleteConfig() {
+bool ConfigService::deleteConfigFile() {
     if (!LittleFS.remove(_configPath)) {
         Serial.printf("ERROR: failed to read config file: %s\n", _configPath);
         return(false);
     }
-    doc->clear();
+    docPtr->clear();
     _println(String("Removed config file: ") + _configPath);
     return(true);
-}
+};
 
-bool ConfigService::validEntry(const String& key) {
-    return ((*doc).containsKey(key) && !(*doc)[key].isNull());
-}
-
-bool ConfigService::_readConfig() {
+bool ConfigService::_readConfigFile() {
     File f = LittleFS.open(_configPath, "r");
     if (!f) {
         Serial.printf("ERROR: failed to read config file: %s\n", _configPath);
         return(false);
     }
-    DeserializationError error = deserializeJson(*doc, f);
+    DeserializationError error = deserializeJson(*docPtr, f);
     if (error) {
-        Serial.print("ERROR: _readConfig() deserializeJson() failed: ");
+        Serial.print("ERROR: _readConfigFile() deserializeJson() failed: ");
         Serial.println(error.f_str());
         return(false);
     };
+    if (docPtr->overflowed()) {
+        Serial.println("ERROR: doc overflowed on read from file");
+        return(false);
+    }
     _println("Read config file and deserialized JSON");
     f.close();
     return(true);
-}
+};
 
 void ConfigService::_printConfigDoc() {
-    serializeJsonPretty(*doc, Serial);
-}
+    serializeJsonPretty(*docPtr, Serial);
+};
 
 void ConfigService::_print(String str) {
   if (_verbose) {
     Serial.print(str);
   }
-}
+};
 
 void ConfigService::_println(String str) {
   if (_verbose) {
     Serial.println(str);
   }
-}
+};

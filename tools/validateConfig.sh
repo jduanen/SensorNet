5d353f2f0261c52de14647dccca784c29d47c7a9
Duane Northcutt <duane.northcutt@gmail.com>
2026-01-21 13:49:59 -0800

    Made use of variables consistent. Made local files explicit list.

--- tools/validateConfig.sh
+++ tools/validateConfig.sh
@@ -1,32 +1,64 @@
 #!/bin/bash
+#
+# Script to validate that all of the config files pass syntax checks
+#
+# Have to be in the ESPHOME venv
+#  workon ESPHOME
+
+SOURCE_DIR="${HOME}/Code/SensorNet/configs/esphome"
 
 SUCCEEDED=
 FAILED=
 
-for c in *.yaml; do
-  esphome -q config ${c}
+LOCAL_FILES=(
+    "feeder-door.yaml"
+    "air-quality-pms-0.yaml"
+    "air-quality-pms-1.yaml"
+    "air-quality-pms-2.yaml"
+    "air-quality-sps-0.yaml"
+    "env-sensors.yaml"
+    "kittycam.yaml"
+    "kittycam2.yaml"
+    "led-sign.yaml"
+    "radiation-0.yaml"
+    "radiation-1.yaml"
+    "smart-plug-ems01-0.yaml"
+    "tempsense0.yaml"
+    "tempsense1.yaml"
+    "water-temperature.yaml"
+    "waterheater-leak-detector.yaml"
+    "home-assistant-voice-0.yaml"
+    "respeaker-xvf3800-0.yaml"
+    "satellite1-cfec40.yaml"
+    "waveshare-audio-0.yaml"
+    "waveshare-audio-1.yaml"
+)
+
+for file in "${LOCAL_FILES[@]}"; do
+  FILE_PATH="$SOURCE_DIR/$file"
+  esphome -q config $FILE_PATH
   if [ $? -ne 0 ]; then
     if [ -z "$FAILED" ]; then
-      FAILED=${c}
+      FAILED=$FILE_PATH
     else
-      FAILED=${FAILED}" "${c}
+      FAILED=$FAILED" "$FILE_PATH
     fi
   else
     if [ -z "$SUCCEEDED" ]; then
-      SUCCEEDED=${c}
+      SUCCEEDED=$FILE_PATH
     else
-      SUCCEEDED=${SUCCEEDED}" "${c}
+      SUCCEEDED=$SUCCEEDED" "$FILE_PATH
     fi
   fi
 done
 
 echo ""
 echo "Succeeded: "
-for s in ${SUCCEEDED}; do
-  echo "    "${s}
+for s in $SUCCEEDED; do
+  echo "    "$s
 done
 echo ""
 echo "Failed: "
-for f in ${FAILED}; do
-  echo "    "${f}
+for f in $FAILED; do
+  echo "    "$f
 done

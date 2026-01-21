425006638f2dd96f1b7d3b04d23c300beca37747
Duane Northcutt <duane.northcutt@gmail.com>
2026-01-21 14:34:44 -0800

    Validate config files in place.

--- tools/validateConfig.sh
+++ tools/validateConfig.sh
@@ -4,61 +4,87 @@
 #
 # Have to be in the ESPHOME venv
 #  workon ESPHOME
+# Also needs to have symlink to secrets.yaml
 
-SOURCE_DIR="${HOME}/Code/SensorNet/configs/esphome"
+LOG_FILE="/tmp/sensornet.log"
+SOURCE_DIR="${HOME}/Code/SensorNet"
+
+CONTROLLERS="${SOURCE_DIR}/controllers"
+SENSORS="${SOURCE_DIR}/sensors"
+VOICE_ASSISTANTS="${SOURCE_DIR}/voiceAssistants"
+
+SOURCE_FILES=(
+    "${CONTROLLERS}/FeederDoor/feeder-door.yaml"
+    "${SENSORS}/AirQualityPMS/air-quality-pms-0.yaml"
+    "${SENSORS}/AirQualityPMS/air-quality-pms-1.yaml"
+    "${SENSORS}/AirQualityPMS/air-quality-pms-2.yaml"
+    "${SENSORS}/AirQualitySPS/air-quality-sps-0.yaml"
+    "${SENSORS}/EnvironmentalSensors/env-sensors.yaml"
+    "${SENSORS}/KittyCam/kittycam.yaml"
+    "${SENSORS}/KittyCamV2/kittycam2.yaml"
+    "${SENSORS}/LedSign/led-sign.yaml"
+    "${SENSORS}/Radiation/radiation-0.yaml"
+    "${SENSORS}/Radiation/radiation-1.yaml"
+    "${SENSORS}/SmartPlugEMS01/smart-plug-ems01-0.yaml"
+    "${SENSORS}/TemperatureDisplay/tempsense0.yaml"
+    "${SENSORS}/Temperature/tempsense1.yaml"
+    "${SENSORS}/WaterHeater/water-temperature.yaml"
+    "${SENSORS}/WaterHeaterLeak/waterheater-leak-detector.yaml"
+    "${VOICE_ASSISTANTS}/HomeAssistantVoicePE/home-assistant-voice-0.yaml"
+    "${VOICE_ASSISTANTS}/ReSpeakerVXF3800Satellite/respeaker-xvf3800-0.yaml"
+    "${VOICE_ASSISTANTS}/Satellite1/satellite1-cfec40.yaml"
+    "${VOICE_ASSISTANTS}/WaveshareSatellite/waveshare-audio-0.yaml"
+    "${VOICE_ASSISTANTS}/WaveshareSatellite/waveshare-audio-1.yaml"
+)
 
 SUCCEEDED=
 FAILED=
 
-LOCAL_FILES=(
-    "feeder-door.yaml"
-    "air-quality-pms-0.yaml"
-    "air-quality-pms-1.yaml"
-    "air-quality-pms-2.yaml"
-    "air-quality-sps-0.yaml"
-    "env-sensors.yaml"
-    "kittycam.yaml"
-    "kittycam2.yaml"
-    "led-sign.yaml"
-    "radiation-0.yaml"
-    "radiation-1.yaml"
-    "smart-plug-ems01-0.yaml"
-    "tempsense0.yaml"
-    "tempsense1.yaml"
-    "water-temperature.yaml"
-    "waterheater-leak-detector.yaml"
-    "home-assistant-voice-0.yaml"
-    "respeaker-xvf3800-0.yaml"
-    "satellite1-cfec40.yaml"
-    "waveshare-audio-0.yaml"
-    "waveshare-audio-1.yaml"
-)
-
-for file in "${LOCAL_FILES[@]}"; do
-  FILE_PATH="$SOURCE_DIR/$file"
-  esphome -q config $FILE_PATH
-  if [ $? -ne 0 ]; then
-    if [ -z "$FAILED" ]; then
-      FAILED=$FILE_PATH
+date > $LOG_FILE
+for file in "${SOURCE_FILES[@]}"; do
+    if [[ -f "$file" ]]; then
+        if [[ -n "$VERBOSE" ]]; then
+            echo -n "    Validating: $file ... "
+        fi
+        esphome -q config $file &>> $LOG_FILE
+        if [[ $? -ne 0 ]]; then
+            if [[ -n "$VERBOSE" ]]; then
+                echo "Failed."
+            fi
+            if [[ -z "$FAILED" ]]; then
+                FAILED=$file
+            else
+                FAILED="$FAILED $file"
+            fi
+            echo "^^^^^^^^ $file ^^^^^^^^" >> $LOG_FILE
+        else
+            if [[ -n "$VERBOSE" ]]; then
+                echo "Succeeded."
+            fi
+            if [[ -z "$SUCCEEDED" ]]; then
+                SUCCEEDED=$file
+            else
+                SUCCEEDED="$SUCCEEDED $file"
+            fi
+        fi
     else
-      FAILED=$FAILED" "$FILE_PATH
+        echo "ERROR: missing file ($file)"
+        exit 1
     fi
-  else
-    if [ -z "$SUCCEEDED" ]; then
-      SUCCEEDED=$FILE_PATH
-    else
-      SUCCEEDED=$SUCCEEDED" "$FILE_PATH
-    fi
-  fi
 done
 
 echo ""
-echo "Succeeded: "
-for s in $SUCCEEDED; do
-  echo "    "$s
-done
-echo ""
-echo "Failed: "
-for f in $FAILED; do
-  echo "    "$f
-done
+if [[ -n $SUCCEEDED ]]; then
+    echo "Succeeded: "
+    for s in $SUCCEEDED; do
+        echo "    "$s
+    done
+    echo ""
+fi
+if [[ -n $FAILED ]]; then
+    echo "Failed: "
+    for f in $FAILED; do
+        echo "    "$f
+    done
+    echo "See logs in: $LOG_FILE"
+fi

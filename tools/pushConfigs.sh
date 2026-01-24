01daad8cb778a7c9cf45d21baeb258c4138fdb04
Duane Northcutt <duane.northcutt@gmail.com>
2026-01-24 13:57:24 -0800

    Refactored and included common code.

--- tools/pushConfigs.sh
+++ tools/pushConfigs.sh
@@ -10,41 +10,9 @@
 # Test with:
 #  ssh -i /root/.ssh/rsyncKey jdn@gpuServer1.local
 
-#### TODO move this to a common file and include it
-
-REMOTE_USER_HOST="jdn@gpuServer1.local"
-REMOTE_DIR="Code/SensorNet"
+source "${dirname "$0"}/common.sh"
 
 LOCAL_DIR="/root/config/esphome"
-
-CONTROLLERS="${REMOTE_DIR}/controllers"
-SENSORS="${REMOTE_DIR}/sensors"
-DISPLAYS="${SOURCE_DIR}/displays"
-VOICE_ASSISTANTS="${REMOTE_DIR}/voiceAssistants"
-
-REMOTE_FILES=(
-    "${CONTROLLERS}/FeederDoor/feeder-door.yaml"
-    "${SENSORS}/AirQualityPMS/air-quality-pms-0.yaml"
-    "${SENSORS}/AirQualityPMS/air-quality-pms-1.yaml"
-    "${SENSORS}/AirQualityPMS/air-quality-pms-2.yaml"
-    "${SENSORS}/AirQualitySPS/air-quality-sps-0.yaml"
-    "${SENSORS}/EnvironmentalSensors/env-sensors.yaml"
-    "${SENSORS}/KittyCam/kittycam.yaml"
-    "${SENSORS}/KittyCamV2/kittycam2.yaml"
-    "${SENSORS}/Radiation/radiation-0.yaml"
-    "${SENSORS}/Radiation/radiation-1.yaml"
-    "${SENSORS}/SmartPlugEMS01/smart-plug-ems01-0.yaml"
-    "${SENSORS}/TemperatureDisplay/tempsense0.yaml"
-    "${SENSORS}/Temperature/tempsense1.yaml"
-    "${SENSORS}/WaterHeater/water-temperature.yaml"
-    "${SENSORS}/WaterHeaterLeak/waterheater-leak-detector.yaml"
-    "${DISPLAYS}/LedSign/led-sign.yaml"
-    "${VOICE_ASSISTANTS}/HomeAssistantVoicePE/home-assistant-voice-0.yaml"
-    "${VOICE_ASSISTANTS}/ReSpeakerVXF3800Satellite/respeaker-xvf3800-0.yaml"
-    "${VOICE_ASSISTANTS}/Satellite1/satellite1-cfec40.yaml"
-    "${VOICE_ASSISTANTS}/WaveshareSatellite/waveshare-audio-0.yaml"
-    "${VOICE_ASSISTANTS}/WaveshareSatellite/waveshare-audio-1.yaml"
-)
 
 KEY="~/.ssh/rsyncKey"
 
--- tools/validateConfigs.sh
+++ tools/validateConfigs.sh
@@ -6,39 +6,9 @@
 #  workon ESPHOME
 # Also needs to have symlink to secrets.yaml
 
-#### TODO move this to a common file and include it
+source "${dirname "$0"}/common.sh"
 
 LOG_FILE="/tmp/sensornet.log"
-SOURCE_DIR="${HOME}/Code/SensorNet"
-
-CONTROLLERS="${SOURCE_DIR}/controllers"
-SENSORS="${SOURCE_DIR}/sensors"
-DISPLAYS="${SOURCE_DIR}/displays"
-VOICE_ASSISTANTS="${SOURCE_DIR}/voiceAssistants"
-
-SOURCE_FILES=(
-    "${CONTROLLERS}/FeederDoor/feeder-door.yaml"
-    "${SENSORS}/AirQualityPMS/air-quality-pms-0.yaml"
-    "${SENSORS}/AirQualityPMS/air-quality-pms-1.yaml"
-    "${SENSORS}/AirQualityPMS/air-quality-pms-2.yaml"
-    "${SENSORS}/AirQualitySPS/air-quality-sps-0.yaml"
-    "${SENSORS}/EnvironmentalSensors/env-sensors.yaml"
-    "${SENSORS}/KittyCam/kittycam.yaml"
-    "${SENSORS}/KittyCamV2/kittycam2.yaml"
-    "${SENSORS}/Radiation/radiation-0.yaml"
-    "${SENSORS}/Radiation/radiation-1.yaml"
-    "${SENSORS}/SmartPlugEMS01/smart-plug-ems01-0.yaml"
-    "${SENSORS}/TemperatureDisplay/tempsense0.yaml"
-    "${SENSORS}/Temperature/tempsense1.yaml"
-    "${SENSORS}/WaterHeater/water-temperature.yaml"
-    "${SENSORS}/WaterHeaterLeak/waterheater-leak-detector.yaml"
-    "${DISPLAYS}/LedSign/led-sign.yaml"
-    "${VOICE_ASSISTANTS}/HomeAssistantVoicePE/home-assistant-voice-0.yaml"
-    "${VOICE_ASSISTANTS}/ReSpeakerVXF3800Satellite/respeaker-xvf3800-0.yaml"
-    "${VOICE_ASSISTANTS}/Satellite1/satellite1-cfec40.yaml"
-    "${VOICE_ASSISTANTS}/WaveshareSatellite/waveshare-audio-0.yaml"
-    "${VOICE_ASSISTANTS}/WaveshareSatellite/waveshare-audio-1.yaml"
-)
 
 SUCCEEDED=
 FAILED=

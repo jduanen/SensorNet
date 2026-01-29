135ef8562d48cc08e97d50dc18ad432686d323ad
Duane Northcutt <duane.northcutt@gmail.com>
2026-01-28 20:35:17 -0800

    Created.

---
+++ tools/patchSAT1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This script patches the FutureProofHomes Satellite1 Home Assistant Voice
+#  Assistant to be compatible with SensorNet.
+#
+
+#### FIXME
+echo "ERROR: must implement patchSAT1"
+exit 1

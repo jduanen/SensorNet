b50b095dd5d2772e65fc27ec53bfd9dd6f94a373
Duane Northcutt <duane.northcutt@gmail.com>
2026-02-05 18:10:16 -0800

    Fixed.

--- tools/patchRSX.sh
+++ tools/patchRSX.sh
@@ -18,50 +18,50 @@
 #      - - platform: wifi_signal
 #          id: wifi_rssi
 #          name: ${friendly_name} WiFi Signal"
+#
+# N.B. Must use go-yq v4 ("mikefarah/yq"). Don't use the one installed by apt. 'sudo snap install yq'
 
-echo "FIXME: corrupts parts with lambdas"
-exit 1
-
-DEBUG=${DEBUG:-false}
+scriptDir="$(cd "$(dirname "$0")" && pwd)"
+source "$scriptDir/commonPatch.sh"
 
 SOURCE_FILE="${HOME}/Code2/Respeaker-XVF3800-ESPHome-integration/config/respeaker-xvf-satellite-example.yaml"
 
-SRC_FILE="/tmp/src.yaml"
-TMP_FILE="/tmp/tmp.yaml"
 DST_FILE="${HOME}/Code/SensorNet/voiceAssistants/ReSpeakerVXF3800Satellite/packages/respeaker-xvf3800.yaml"
 
-updateYaml() {
-    local opts="$1"
-    shift
-    local file="$1"
-    shift
+checkYQ
 
-    if ! yq $opts "$@" "$file"; then
-        echo "ERROR: yq failed on file '$file'"
-        exit 1
-    fi
-}
-
-updateYaml -yr $SOURCE_FILE '.' > $SRC_FILE
-cp $SRC_FILE $TMP_FILE
-if [[ "$DEBUG" == "true" ]]; then
-    ls -l $SRC_FILE $TMP_FILE
+# pre-process YAML
+YAML_FILE=$(mktemp --suffix=.yaml src-XXXX)
+if ! cat $SOURCE_FILE | sed -E "s@(\x21lambda) '(.+)'@\1\2@" | sed -E "s/(\x21lambda .*$)/\'\1\'/"  > $YAML_FILE; then
+    echo "ERROR: yaml file preprocessing failed"
+    exit 1
 fi
-
-#### FIXME combine operators for efficiency
-#yq -yri '(.esphome.name = "${device_name}") | (.esphome.name_add_mac_suffix = false) , (.wifi.ssid = "!secret wifi_ssid") | (.wifi.password = "!secret wifi_password")' $TMP_FILE
-updateYaml -yri $TMP_FILE '(.esphome.name = "${device_name}")'
-updateYaml -yri $TMP_FILE '(.esphome.friendly_name = "${friendly_name}")'
-updateYaml -yri $TMP_FILE '(.esphome.comment = "${comment}")'
-updateYaml -yri $TMP_FILE '(.logger.level = "${log_level}")'
-updateYaml -yri $TMP_FILE '(.sensor += [{"platform": "wifi_signal", "id": "wifi_rssi", "name": "${friendly_name} WiFi Signal"}])'
-
-cp $TMP_FILE $DST_FILE
-
-if [[ "$DEBUG" == "true" ]]; then
-    echo ""
-    ls -l $SRC_FILE $TMP_FILE
-    tkdiff $SRC_FILE $TMP_FILE
+if [[ "$DEBUG" == true ]]; then
+    diff $SOURCE_FILE $YAML_FILE
 else
-    rm $SRC_FILE $TMP_FILE
+    rm -f $YAML_FILE
 fi
+
+convertToJson $YAML_FILE $SRC_FILE
+
+# pre-process JSON
+
+# edit JSON
+updateJson "$SRC_FILE" "$TMP_FILE" '(.esphome.name = "${device_name}") | 
+    (.esphome.friendly_name = "${friendly_name}") |
+    (.esphome.comment = "${comment}") |
+    (.wifi.ssid = "!secret wifi_ssid") |
+    (.wifi.password = "!secret wifi_password") |
+    (.logger.level = "${log_level}") |
+    (.sensor += [{"platform": "wifi_signal", "id": "wifi_rssi", "name": "${friendly_name} WiFi Signal"}])'
+if [[ "$DEBUG" == true ]]; then
+    diff $SRC_FILE $TMP_FILE
+fi
+
+# convert to YAML and post-process it
+if ! yq -P -o=yaml '.' $TMP_FILE | sed -E "s/'(\x21.* [^']*)'/\1/g" > $DST_FILE; then
+    echo "ERROR: failed to convert file back to YAML"
+    exit 1
+fi
+
+# cleanup

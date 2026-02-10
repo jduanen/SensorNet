d397c773ac8e3bd312293477a9f04ce20261e388
Duane Northcutt <duane.northcutt@gmail.com>
2026-02-10 13:26:50 -0800

    Added comment.

--- tools/patchSAT1.sh
+++ tools/patchSAT1.sh
@@ -3,6 +3,8 @@
 # This script patches the FutureProofHomes Satellite1 Home Assistant Voice
 #  Assistant to be compatible with SensorNet.
 #
+# N.B. Must use go-yq v4 ("mikefarah/yq"). Don't use the one installed by apt. 'sudo snap install yq'
+
 
 #### FIXME
 echo "ERROR: must implement patchSAT1"

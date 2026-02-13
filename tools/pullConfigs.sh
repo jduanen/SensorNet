6d238d15593938834c960607a793ef106c303a17
Duane Northcutt <duane.northcutt@gmail.com>
2026-02-13 10:51:24 -0800

    Fixed remote files. Added debug print.

--- tools/pullConfigs.sh
+++ tools/pullConfigs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Script to pull ESPHome config files from gpuServer1.lan to HA Server
+# Script to pull ESPHome config files from remote server to HA Server
 #
 # Create key on HA server first:
 #  ssh-keygen -t ed25519 -f ~/.ssh/rsyncKey -C "rsync automation key"
@@ -10,6 +10,8 @@
 # Test with:
 #  ssh -i /root/.ssh/rsyncKey jdn@gpuServer1.local
 
+trap 'echo "Interrupted by Ctrl+C"; exit 130' INT
+
 SCRIPT_DIR="$(dirname "$0")"
 source "${SCRIPT_DIR}/common.sh"
 
@@ -17,14 +19,10 @@ LOCAL_DIR="/root/config/esphome"
 
 KEY="~/.ssh/rsyncKey"
 
-trap 'echo "Interrupted by Ctrl+C"; exit 130' INT
-
-for remoteFile in "${REMOTE_FILES[@]}"; do
-    localFile="${remoteFile##*/}"
-    if [[ -f "$localFile" ]]; then
-        rsync -avqz -e "ssh -i $KEY" --protect-args "$REMOTE_USER_HOST:$remoteFile" "$localFile"
-    else
-        echo "ERROR: Missing file ($localFile)"
-        exit 1
+for remoteFile in "${SOURCE_FILES[@]}"; do
+    localFile="$LOCAL_DIR/${remoteFile##*/}"
+    if [[ "$DEBUG" == true ]]; then
+        echo "$remoteFile -> $localFile"
     fi
+    rsync -avqz -e "ssh -i $KEY" --protect-args "$REMOTE_USER_HOST:$remoteFile" "$localFile"
 done
